bdc09f8059cd32209e21755feedcd1825d3e47aa
Nithin Gopal <nithingopal2015@gmail.com>
2022-04-01 19:11:20 +0530

    Fix style and elements
    - fixed numkey issue
    - added styles
    -operations pending

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@
 
 const body = document.querySelector('body');
 const mainBox = document.createElement('div');
-const heading = document.createElement('h2');
+//const heading = document.createElement('h2');
 
-heading.textContent = 'Calculator';
-body.appendChild(heading);
+//heading.textContent = 'Calculator';
+//body.appendChild(heading);
 mainBox.setAttribute('id', 'mainBox')
 body.appendChild(mainBox);
 
@@ -47,41 +47,67 @@ function build () {
     //declare/create num,operate elements 
     let numContainer = document.createElement('div');
     let operateContainer = document.createElement('div');
-    let zeroDotEqualContainer = document.createElement('div');
     let numKeyContainer = document.createElement('div');
     //add classes to them
     numContainer.classList.add('numContainer');
     operateContainer.classList.add('operateContainer');
-    zeroDotEqualContainer.classList.add('zeroDotEqualContainer');
     numKeyContainer.classList.add('numKeyContainer');
-    //add to num keys and 0/./= keys to numcontainer
-    numContainer.appendChild(zeroDotEqualContainer);
+    //add to numRows and 0/./= Row to numcontainer
+    let numRow1 = document.createElement('div');
+    let numRow2 = document.createElement('div');
+    let numRow3 = document.createElement('div');
+    numRow1.classList.add('numRow1');
+    numRow2.classList.add('numRow2');
+    numRow3.classList.add('numRow3');
+    numKeyContainer.appendChild(numRow1);
+    numKeyContainer.appendChild(numRow2);
+    numKeyContainer.appendChild(numRow3);
+    let zeroDotEqualContainer = document.createElement('div');
+    zeroDotEqualContainer.classList.add('zeroDotEqualContainer');
+    numKeyContainer.appendChild(zeroDotEqualContainer);
     numContainer.appendChild(numKeyContainer);
     //add numContainer and operateContainer to main keyContainer
     keyContainer.appendChild(numContainer);
     keyContainer.appendChild(operateContainer);
     //populate number keys and add to numkeycontainer
-    for (k=9; k>=1; k--) {
+    for (k=0; k<12; k++) {
         let numKeys = document.createElement('button');
         numKeys.classList.add('numKeys');
-        numKeys.textContent = +k;
-        numKeyContainer.appendChild(numKeys);
+        if (k === 11) {
+            numKeys.textContent = '=';
+            zeroDotEqualContainer.appendChild(numKeys);
+        }else if (k === 0) {
+            numKeys.textContent = 0;
+            zeroDotEqualContainer.appendChild(numKeys);
+        }else if (k === 10) {
+            numKeys.textContent = '.';
+            zeroDotEqualContainer.appendChild(numKeys);
+        }else if (k ===9 || k === 8 || k ===7) {
+            numKeys.textContent = k;
+            numRow1.appendChild(numKeys);
+        }else if (k === 6 || k === 5 || k === 4) {
+            numKeys.textContent = k;
+            numRow2.appendChild(numKeys);
+        }else if (k === 3 || k === 2 || k === 1) {
+            numKeys.textContent = k;
+            numRow3.appendChild(numKeys);
+        }
     }
     //populate 0/./= keys and add to zeroDotEqualContainer
-    for (l=0; l<3; l++) {
-        let zeroDotEqualKeys = document.createElement('button');
-        zeroDotEqualKeys.classList.add('numKeys');
-        //assign text content
-        if (l === 0 ) {
-                zeroDotEqualKeys.textContent = '.';
-            } 
-        else if (l === 1) {
-             zeroDotEqualKeys.textContent = 0;
-        }else if (l === 2) {
-             zeroDotEqualKeys.textContent = '=';
-         }
-        zeroDotEqualContainer.appendChild(zeroDotEqualKeys);
-    }
+    // for (l=0; l<3; l++) {
+    //     let zeroDotEqualKeys = document.createElement('button');
+    //     zeroDotEqualKeys.classList.add('numKeys');
+    //     //assign text content
+    //     if (l === 0 ) {
+    //             zeroDotEqualKeys.textContent = '.';
+    //         } 
+    //     else if (l === 1) {
+    //          zeroDotEqualKeys.textContent = 0;
+    //     }else if (l === 2) {
+    //          zeroDotEqualKeys.textContent = '=';
+    //      }
+    //     zeroDotEqualContainer.appendChild(zeroDotEqualKeys);
+    // }
     //populate operation keys and add to operateContainer
     for (m=0; m<4; m++){
         let operateKey = document.createElement('button');
